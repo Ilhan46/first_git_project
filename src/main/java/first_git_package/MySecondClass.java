28113435376c03d259c98aa3e7f20005e972d01f
Ilhan Dursun <ilhanndursun@gmail.com>
2023-03-11 18:21:18 +0100

    Updating second class

--- src/main/java/first_git_package/MySecondClass.java
+++ src/main/java/first_git_package/MySecondClass.java
@@ -4,5 +4,7 @@ public class MySecondClass {
     public static void main(String[] args) {
 
         System.out.println("Second class");
+
+        System.out.println("Updating existing code");
     }
 }
